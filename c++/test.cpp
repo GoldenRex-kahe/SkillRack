ca3307001fd40e70cad2f93ca84169a649fb96c2
GoldenRex-kahe <ffrex81842@gmail.com>
2026-07-15 22:47:04 +0530

    Fix C and C++ build setup

---
+++ c++/test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "C++ is running!" << std::endl;
+    return 0;
+}
---
+++ c/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("C is running!\n");
+    return 0;
+}
