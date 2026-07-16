0e71e4724cc78e8b84bc8e6f33a477089dc1a684
GoldenRex-kahe <ffrex81842@gmail.com>
2026-07-16 03:57:59 +0000

    Jul 16, 2026, 9:27 AM

--- c++/Find the max value.cpp
+++ c++/Find the max value.cpp
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    
+    int n,maxv,curr;
+    scanf("%d%d", &n, &maxv);
+    for(int count=2;count<=n;count++)
+    {
+        scanf("%d", &curr);
+        maxv=max(maxv,curr);
+    }
+    printf("%d", maxv);
 }
