163a0ad73bd77263867333d6f7b01e62751f42fe
GoldenRex-kahe <ffrex81842@gmail.com>
2026-07-17 09:20:48 +0000

    Jul 17, 2026, 2:50 PM

--- c/Static Range Sum Queries.c
+++ c/Static Range Sum Queries.c
@@ -6,14 +6,14 @@ int main() {
 	prefix[0]=0;
 	for(int i=1;i<=as;i++){
 	    long long value;
-	    scanf("%d",&value);
+	    scanf("%lld",&value);
 	    prefix[i]=prefix[i-1]+value;
 	}
 	for(int i=0;i<rs;i++){
 	    int left,right;
 	    scanf("%d%d",&left,&right);
 	    long long sum=prefix[right]-prefix[left-1];
-	    printf("%d\n",sum);
+	    printf("%lld\n",sum);
 	}
 	return 0;
 
